b78ff47671addd15409fc8e47b5d218f70f2245d
DFerenczi <danielferenczi@hotmail.com>
2014-04-03 12:05:19 +0200

    Testscene, movement first iter.

---
+++ Assets/CtrlScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CtrlScript : MonoBehaviour 
+{
+	//src_touchInput touchIn = null;
+	public float speed = 1;
+	public Vector2 testVec;
+	public float rotation = 1;
+	Vector3 rDirVec = new Vector3(0.0f, 0.0f, 0.0f);
+	Vector3 lDirVec = new Vector3(0.0f, 0.0f, 0.0f);
+	Vector3 finalDirVec = new Vector3(0.0f, 0.0f, 0.0f);
+
+	// Use this for initialization
+	void Start () 
+	{
+		//testVec = new Vector2 (1.0f, 1.0f);
+//		touchIn = GetComponent<src_touchInput> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		rDirVec.z = testVec.y;
+		lDirVec.z = testVec.x;
+		finalDirVec = rDirVec + lDirVec;
+
+		//Adding rotation..
+		transform.Rotate (finalDirVec.y);
+		//transform.rotation = Quaternion.FromToRotation(,finalDirVec ;
+
+		rigidbody.AddForce (finalDirVec * speed, ForceMode.VelocityChange);
+	}
+}
