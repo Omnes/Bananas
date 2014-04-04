b7570095316448e57d1fb3ab8aaa43900a757329
DFerenczi <danielferenczi@hotmail.com>
2014-04-04 11:14:11 +0200

    More or less finished movement

--- Assets/CtrlScript.cs
+++ Assets/CtrlScript.cs
@@ -3,32 +3,49 @@ using System.Collections;
 
 public class CtrlScript : MonoBehaviour 
 {
-	//src_touchInput touchIn = null;
-	public float speed = 1;
-	public Vector2 testVec;
-	public float rotation = 1;
-	Vector3 rDirVec = new Vector3(0.0f, 0.0f, 0.0f);
-	Vector3 lDirVec = new Vector3(0.0f, 0.0f, 0.0f);
-	Vector3 finalDirVec = new Vector3(0.0f, 0.0f, 0.0f);
+	src_touchInput touchIn = null;
+	public float speed;
+
+	public Vector2 testVec = new Vector2(1.0f, 0.5f);
+	Vector3 targetVec = new Vector3(0.0f, 0.0f, 0.0f);
+	Vector3 finalDirVec = new Vector3(0, 0, 0);
+	Vector3 tmpVec;
+	float right;
+	float left;
+	float diff;
+	float angleToNew;
+	float angleFactor;
 
 	// Use this for initialization
 	void Start () 
 	{
 		//testVec = new Vector2 (1.0f, 1.0f);
-//		touchIn = GetComponent<src_touchInput> (); 
+		touchIn = GetComponent<src_touchInput> (); 
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		rDirVec.z = testVec.y;
-		lDirVec.z = testVec.x;
-		finalDirVec = rDirVec + lDirVec;
+
+		right = testVec.y;
+		left = testVec.x;
+
+
+		float tmpSpeed =  (right + left)/2;
+		diff = left - right;
+		angleFactor = diff / 0.1f; //always comparing to 0.1(9 degrees)
+		angleToNew = 9 * angleFactor; //getting the angle to the new direction through multiplying 9 with the factor calculated above
+
+		Vector3 currDir = transform.rotation.eulerAngles;
+		Vector3 temp = Vector3.down * left + Vector3.up * right;
 
 		//Adding rotation..
-		transform.Rotate (finalDirVec.y);
-		//transform.rotation = Quaternion.FromToRotation(,finalDirVec ;
+//		finalDirVec = Vector3.RotateTowards (transform.forward, targetVec, angleToNew, 0.0f);
+//		Debug.DrawRay (transform.position, finalDirVec, Color.red);
+//		transform.Rotate = Quaternion.AngleAxis(angleToNew, transform.up); 
 
-		rigidbody.AddForce (finalDirVec * speed, ForceMode.VelocityChange);
+		transform.Rotate (temp);
+
+		rigidbody.AddForce (transform.forward * speed * tmpSpeed, ForceMode.VelocityChange);
 	}
 }
