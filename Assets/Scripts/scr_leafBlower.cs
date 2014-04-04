9673d9dfcb2cd4ffc5e2fb4a89d28d426a31859c
Johannes Sinander <johannes.sinander@gmail.com>
2014-04-04 16:36:41 +0200

    Saker med leafBlower

--- Assets/Scripts/scr_leafBlower.cs
+++ Assets/Scripts/scr_leafBlower.cs
@@ -6,8 +6,14 @@ public class scr_leafBlower : MonoBehaviour {
 	public float m_blowPower = 0.0f;
 	public float m_forwardPower = 1.0f;
 	public float m_centripetalPower = 1.0f;
+	public float m_torquePower = 0.0f;
 //	public float m_powerVariation = 0.0f;
 //	public bool m_blowStraight
+
+	public float m_minVelocity = 0.0f;
+	public float m_centDistMult = 0.0f;
+
+	private float debug_angle = 0.0f;
 
 	private scr_touchInput m_touchInput;
 
@@ -32,19 +38,37 @@ public class scr_leafBlower : MonoBehaviour {
 
 			//Centreipetal power
 			Vector3 playerDirection = transform.parent.TransformDirection( -Vector3.forward );	
-			Vector3 projection = transform.parent.position + Vector3.Project(leaf.transform.position - transform.parent.position, playerDirection);
+			Vector3 projectionPoint = transform.parent.position + Vector3.Project(leaf.transform.position - transform.parent.position, playerDirection);
 
-			Vector3 projectionDirection = (projection - leaf.transform.position).normalized;
+			Vector3 projectionDirection = projectionPoint - leaf.transform.position;
+			float distance = projectionDirection.magnitude;
+			projectionDirection.Normalize();
 //			scr_leafPhysics.AddForce(projectionDirection * m_centripetalPower * m_blowPower);
-			leaf.rigidbody.AddForce(projectionDirection * m_centripetalPower * m_blowPower);
+			leaf.rigidbody.AddForce(projectionDirection * m_centripetalPower * m_blowPower * (1 + distance * m_centDistMult));
 
 			//Blow power
-			Transform blow_point = transform.parent.FindChild("blow_point");
-			if (blow_point != null) {
-				Vector3 directionVector = (leaf.transform.position - blow_point.position).normalized;
+//			Transform blow_point = transform.parent.FindChild("blow_point");
+//			if (blow_point != null) {
+//				Vector3 directionVector = (leaf.transform.position - blow_point.position).normalized;
+				Vector3 directionVector = (leaf.transform.position - transform.parent.position).normalized;
 //				scr_leafPhysics.AddForce(directionVector * m_forwardPower * m_blowPower);
 				leaf.rigidbody.AddForce(directionVector * m_forwardPower * m_blowPower);
-			}
+
+//				Mathf.DeltaAngle( transform.
+//				leaf.rigidbody.AddTorque(directionVector * m_torquePower * m_blowPower);	//TODO: Max torque
+
+				Vector3 forwardVector = transform.TransformDirection( -Vector3.forward ).normalized;
+				debug_angle = signedAngle(new Vector2(forwardVector.x, forwardVector.z), new Vector2(directionVector.x, directionVector.z));
+				//debug_angle -= signedAngle(new Vector2(0,0), new Vector2(forwardVector.x, forwardVector.z));
+//				debug_angle = Vector3.Angle( forwardVector, directionVector );
+//				Debug.DrawLine( new Vector3(0,0,0), directionVector, Color.cyan );
+//				Debug.DrawLine( new Vector3(0,0,0), forwardVector, Color.magenta );
+//			}
+
+//			if ( leaf.rigidbody.velocity.magnitude < m_centDistMult ) {
+//				leaf.rigidbody.velocity = leaf.rigidbody.velocity.normalized * m_centDistMult;
+//			}
+
 
 			//leaf.transform.position += transform.parent.transform.TransformDirection( Vector3.back ) * (power + Random.Range(0, powerVariation)) * Time.deltaTime;
 //			Vector3 targetPosition = Vector3.MoveTowards(leaf.transform.position, transform.position, power);
@@ -54,6 +78,17 @@ public class scr_leafBlower : MonoBehaviour {
 		}
 
 	}
+
+	float signedAngle(Vector2 v0, Vector2 v1) {
+//		return Mathf.Atan2 (v0.y - v1.y, v0.x - v1.y) * 180 / Mathf.PI;
+//		return Mathf.Acos (Vector2.Dot(v0, v1)) * 180 / Mathf.PI;
+		return Vector2.Angle (v0, v1);
+
+	}
+
+//	void OnGUI(){
+//		GUI.Label(new Rect(10, 25, 200, 25), "Angle: " + debug_angle.ToString("F2"));
+//	}
 
 //	void OnTriggerExit(Collider col)
 //	{
--- Assets/Scripts/scr_scoreKeeper.cs
+++ Assets/Scripts/scr_scoreKeeper.cs
@@ -4,13 +4,17 @@ using System.Collections;
 public class scr_scoreKeeper : MonoBehaviour {
 	public int m_score = 0;
 
-	// Use this for initialization
-	void Start () {
-	
+	void OnGUI(){
+		GUI.Label(new Rect(10, 10, 100, 25), "Score: " + m_score);
 	}
+
+	// Use this for initialization
+//	void Start () {
+//	
+//	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+//	void Update () {
+//	
+//	}
 }
