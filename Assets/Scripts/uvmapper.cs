8b8db24bc004ff60504052f3593925ec0616fc6d
Robin Iderström <robin.iderstrom@live.se>
2014-04-07 14:54:01 +0200

    uv map test

---
+++ Assets/Scripts/uvmapper.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class uvmapper : MonoBehaviour {
+
+	public Rect map = new Rect(0,0,1,1);
+
+	private MeshFilter meshFilter;
+
+	// Use this for initialization
+	void Start () {
+		meshFilter = GetComponent<MeshFilter>();
+		changeUvs();
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.W)){
+			changeUvs();
+		}
+	}
+
+	void changeUvs(){
+		Mesh mesh = meshFilter.mesh;
+		Vector2[] uvs = new Vector2[4];
+		float l = map.x;
+		float r = map.x+map.width;
+		float u = map.y;
+		float d = map.y+map.height;
+		
+		uvs[0] = new Vector2(l,u); 
+		uvs[1] = new Vector2(r,u);
+		uvs[2] = new Vector2(l,d); 
+		uvs[3] = new Vector2(r,d); 
+		mesh.uv = uvs;
+		mesh.RecalculateNormals();
+		meshFilter.mesh = mesh;
+
+	}
+}
