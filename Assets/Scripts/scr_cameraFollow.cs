cbe979d6415051669e6984808116f2358b4a1a93
Robin Nordholm <robinnordholm@hotmail.com>
2014-04-08 09:48:45 +0200

    Isometric camera test

--- Assets/Scripts/scr_cameraFollow.cs
+++ Assets/Scripts/scr_cameraFollow.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class scr_cameraFollow : MonoBehaviour {
 	public Transform m_target;
 	public float m_distance = 5f;
-	public float m_xdistance= 10f;
+	public float m_zdistance= 10f;
+	public float m_xdistance = 2f;
 	
 	void Update () {
-		transform.position = new Vector3 (m_target.position.x, m_target.position.y + m_distance, m_target.position.z-m_xdistance);
+		transform.position = new Vector3 (m_target.position.x-m_xdistance, m_target.position.y + m_distance, m_target.position.z-m_zdistance);
 	}
 }
+ 
