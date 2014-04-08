a31c605af608628ca95dc9d8ab063f3aa7e55b3a
Arkbrik <andreas.folkesten@hotmail.com>
2014-04-08 11:52:30 +0200

    Kamera, tack viktor

--- Assets/Scripts/scr_cameraFollow.cs
+++ Assets/Scripts/scr_cameraFollow.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class scr_cameraFollow : MonoBehaviour {
 	public Transform m_target;
 	public float m_distance = 5f;
+	public float m_xdistance = -2f;
+	public float m_zdistance =3f;
 	
 	void Update () {
-		transform.position = new Vector3 (m_target.position.x, m_target.position.y + m_distance, m_target.position.z);
+		transform.position = new Vector3 (m_target.position.x-m_xdistance, m_target.position.y + m_distance, m_target.position.z-m_zdistance);
 	}
 }
