3b83f6f2b08d02c5a419c3b84032ef9fe27aab69
Robin Iderström <robin.iderstrom@live.se>
2014-04-11 12:07:25 +0200

    separated collision into its ownsript

---
+++ Assets/Scripts/playerCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerCollision : MonoBehaviour {
+
+	public scr_movementLogic m_movementLogic;
+	public float m_dizzyTime = 6f;
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+		//Check if the "other player" has higher speed .. 
+			if (other.rigidbody.velocity.sqrMagnitude > rigidbody.velocity.sqrMagnitude) //if other speed is higher than mine
+			{
+				//If so make YOUR character "slidable" for 6 sec..
+				m_movementLogic.setTackled();
+				m_movementLogic.Invoke ("restoreMovement", m_dizzyTime);
+				
+			}
+		}
+	}
+}
