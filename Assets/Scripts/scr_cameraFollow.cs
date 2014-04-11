dabffbcae69d250161fd238b70f590b7d85f9a80
Arkbrik <andreas.folkesten@hotmail.com>
2014-04-11 11:05:31 +0200

    Kamera like a boss

--- Assets/Scripts/scr_cameraFollow.cs
+++ Assets/Scripts/scr_cameraFollow.cs
@@ -4,13 +4,10 @@ using System.Collections;
 public class scr_cameraFollow : MonoBehaviour {
 	public Transform m_target;
 	public float m_distance = 5f;
-<<<<<<< HEAD
+
 	public float m_xdistance = -2f;
 	public float m_zdistance =3f;
-=======
-	public float m_zdistance= 10f;
-	public float m_xdistance = 2f;
->>>>>>> origin/viktor
+
 	
 	void Update () {
 		transform.position = new Vector3 (m_target.position.x-m_xdistance, m_target.position.y + m_distance, m_target.position.z-m_zdistance);
