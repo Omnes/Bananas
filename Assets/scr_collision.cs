eaf95064dc465340831270f02f4ffd3c48742a12
Daniel <danielferenczi@hotmail.com>
2014-04-11 10:43:29 +0200

    Kind of collision, movement almost finished, added parameteters..

--- Assets/scr_collision.cs
+++ Assets/scr_collision.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class scr_collision : MonoBehaviour {
+public class scr_collision : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
+
+
+
+//	// Use this for initialization
+//	void Start () {
+//
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
 }
--- Assets/scr_tmpMovementLowSpeed.cs
+++ Assets/scr_tmpMovementLowSpeed.cs
@@ -1,24 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class scr_movementLogic : MonoBehaviour 
+public class scr_tmpMovementLowSpeed : MonoBehaviour 
 {
-	public float m_acceleration;
+	public float m_accelerationProportion;
 	public float m_rotateProportion = 1.0f;
 	public float m_frictionProportion = 0.95f;
 	public float m_minimumSpeed = 0.01f;
 	public float m_maxSpeed = 1.0f;
 
-	private bool hasCollided = false;
 	private Vector3 direction = new Vector3();
+	private bool hasCollided = false;
 	private float right = 0.0f;
 	private float left = 0.0f;
 	private float m_speed;
 	private Vector3 m_finalVelocity;
 	private Vector2 m_inputVec;
 	private scr_touchInput m_touchIn = null;
-
-
+	
+	
 	// Use this for initialization
 	void Start () 
 	{
@@ -31,57 +31,36 @@ public class scr_movementLogic : MonoBehaviour
 		m_inputVec = m_touchIn.getCurrentInputVector ();
 		right = m_inputVec.y;
 		left = m_inputVec.x;
-
+		
 		//friction when no input
 		if (Mathf.Abs(left + right) < m_minimumSpeed) 
 		{
 			rigidbody.velocity *= m_frictionProportion;
 		}
-
-		m_speed = (right + left)/m_acceleration;
-
+		
+		m_speed = (right + left)/m_accelerationProportion;
+		
 		//Adding rotation..
 		Vector3 temp = Vector3.up * left + Vector3.down * right;
 		temp *= m_rotateProportion;
 		transform.Rotate (temp);
-
-
+		
+		
 		//Adding a maximun speed to the character..
 		rigidbody.velocity = Vector3.ClampMagnitude (rigidbody.velocity, m_maxSpeed);
-
-		//Projecting the velocity of the RB to the current(new) direction, this prevents the RB from sliding while turning.. 
+		
+		//Projecting the velocity of the RB to the current(new) direction, this prevents the RB from sliding while turning..
 		if (hasCollided == false) 
 		{
 			direction = transform.forward;
 			rigidbody.velocity = Vector3.Project (rigidbody.velocity, direction.normalized);
 		}
-		else
-		{
-			direction = transform.forward;
-		}
-
+		
 		direction *= m_speed;
 		rigidbody.AddForce (direction, ForceMode.VelocityChange);
-	
+		
 	}
-	void OnCollisionEnter(Collision aCollision)
+	void OnCollisionEnter()
 	{
-		//Check if the "other player" has higher speed .. 
-		if (aCollision.collider.attachedRigidbody.velocity.z > this.rigidbody.velocity.z)
-		{
-			//If so make YOUR character "slidable" for 6 sec..
-			Debug.Log("hasCollided true");
-			hasCollided = true;
-			Invoke ("restoreMovement", 6);
-			Debug.Log("hasCollided false");
-			Debug.Log ("Collision");
-
-		}
 	}
-	void restoreMovement()
-	{
-		hasCollided = false;
-	}
-
-
 }
