fee42e9e1f4878660cc046e94a2ba70aa0e1578c
Sean <conk3r_xd@hotmail.com>
2014-04-24 10:54:45 +0200

    lobby ar typ klar kanske

--- Assets/Scripts/Network/playerData.cs
+++ Assets/Scripts/Network/playerData.cs
@@ -1,19 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class PlayerData : MonoBehaviour {
+public class PlayerData : System.Object {
 
-	public int id;
+	public int m_id;
+	//public string m_ip;
+	public string m_name;
+	//islocal
+
+
 	//rating
 	//apm
 	//characterType
 
-	void Start () {
-		//fungerar det att sätta dontdestroy på det egna objectet?
-		DontDestroyOnLoad(gameObject);
+	//kanske göra en egen serilizable calss med alla info
+	//public List <> connectedPlayers = new List <prefab_player>();
+
+	public PlayerData(string name, int id) {
+		m_name = name;
+		m_id = id;
 	}
 
-	void Update () {
-	
-	}
 }
