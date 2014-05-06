4bfd71ef445aee27838e804eebd9c53966087bef
Robin Iderström <robin.iderstrom@live.se>
2014-05-06 11:36:51 +0200

    prepare for testing

--- Assets/Scripts/RemoteMovement.cs
+++ Assets/Scripts/RemoteMovement.cs
@@ -7,6 +7,17 @@ public class RemoteMovement : MonoBehaviour {
 	private Rigidbody m_rigidbody;
 	private Transform m_transform;
 
+	private Quaternion m_ghostRotation;
+
+	private float m_resyncLimit = 0.05f;
+	public float m_resyncForce = 5f;
+
+	//only temp for debug
+	private float share;
+	private float max;
+	private Vector3 newRotationSpeed;
+	private Vector3 predictedDelta;
+
 //	private bool initiated = false;
 	// Use this for initialization
 	void Start () {
@@ -15,15 +26,39 @@ public class RemoteMovement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 //		transform.Rotate(m_rotationDelta*Time.deltaTime);
+//		predictedDelta = m_ghostRotation.eulerAngles - transform.rotation.eulerAngles; 
+//
+//
+//		max = m_rotationDelta.magnitude + predictedDelta.magnitude;
+//		if(max < m_resyncLimit){
+//			max = 1f;
+//		}
+//		share = m_rotationDelta.magnitude / max;
+//
+//		
+//		newRotationSpeed = m_rotationDelta * share + predictedDelta * m_resyncForce * (1-share);
+
 
 		Quaternion prevAngle = m_transform.rotation;
 		Quaternion newAngle = Quaternion.Euler(prevAngle.eulerAngles + m_rotationDelta * Time.deltaTime);
 		m_rigidbody.MoveRotation(newAngle);
 	}
 
-	public void setSyncRotationSpeed(float rotSpeed){
+	public void setGhostRotation(Quaternion rot,float rotSpeed){
+		m_ghostRotation = rot;
+		transform.rotation = rot; //migth need to be masked
 		m_rotationDelta = new Vector3(0,rotSpeed,0);
+	}
+
+	void OnDrawGizmos(){
+		if(Network.isClient){
+			Gizmos.color = Color.white;
+			float len = 4f;
+			Vector3 pos = transform.position + Vector3.up*0.5f;
+			Gizmos.color = Color.blue;
+			Gizmos.DrawLine(pos,pos + transform.forward*len);
+		}
 	}
 }
