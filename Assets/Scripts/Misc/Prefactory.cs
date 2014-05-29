b245b000b2bd9443a8bf3288b96b77819b088b3a
Johannes Sinander <johannes.sinander@gmail.com>
2014-05-29 12:15:35 +0200

    Fixat merge fel

--- Assets/Scripts/Misc/Prefactory.cs
+++ Assets/Scripts/Misc/Prefactory.cs
@@ -10,9 +10,6 @@ public class Prefactory : MonoBehaviour {
 	public GameObject m_EMPHit;
 	public GameObject m_powerupPickup;
 
-	public Texture2D m_muteButton;
-	public Texture2D m_muteButton2;
-
 	public Gradient m_rainbowColor;
 	public Texture2D m_buttonAtlas;
 
@@ -34,9 +31,6 @@ public class Prefactory : MonoBehaviour {
 	public static GameObject prefab_EMP;
 	public static GameObject prefab_EMPHit;
 	public static GameObject prefab_powerupPickup;
-
-	public static Texture2D texture_muteButton;
-	public static Texture2D texture_muteButton2;
 
 	public static Gradient gradient_rainbowColor;
 
@@ -60,9 +54,6 @@ public class Prefactory : MonoBehaviour {
 		prefab_EMP = m_EMP;
 		prefab_EMPHit = m_EMPHit;
 		prefab_powerupPickup = m_powerupPickup;
-
-		texture_muteButton = m_muteButton;
-		texture_muteButton2 = m_muteButton2;
 
 		gradient_rainbowColor = m_rainbowColor;
 		texture_buttonAtlas = m_buttonAtlas;
