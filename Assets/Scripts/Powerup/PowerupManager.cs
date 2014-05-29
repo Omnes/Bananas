b9dc5aa0adeedd2252cea785d6ac6308b267d7de
Johannes Sinander <johannes.sinander@gmail.com>
2014-05-29 17:12:04 +0200

    Fixat så att man inte kan få bomber vid fel tid eller flera gånger (FÖR 123193 GÅNGEN)

--- Assets/Scripts/Powerup/PowerupManager.cs
+++ Assets/Scripts/Powerup/PowerupManager.cs
@@ -8,10 +8,10 @@ using System.Collections.Generic;
  */
 public class PowerupManager : MonoBehaviour {
 	//Design parameters
-	private const float INIT_SPAWN_DELAY_MIN = 1.0f;
-	private const float INIT_SPAWN_DELAY_MAX = 1.0f;
-	private const float SPAWN_INTERVALL_MIN = 1.0f;
-	private const float SPAWN_INTERVALL_MAX = 1.0f;
+	private const float INIT_SPAWN_DELAY_MIN = 15.0f;
+	private const float INIT_SPAWN_DELAY_MAX = 15.0f;
+	private const float SPAWN_INTERVALL_MIN = 15.0f;
+	private const float SPAWN_INTERVALL_MAX = 18.0f;
 
 	private const int MAX_POWERUPS = 2;
 	private const float SPAWN_RADIUS = 6f;
